8d60834199fb8cf8a548c5b076b56f1f25ce802b
Miguel Escriva <miesgre@gmail.com>
2019-04-05 07:04:13 +0000

    Update paella player develop
    Triggered by https://github.com/polimediaupv/paella/commit/3bad408b2a9ff858ce01320a489a5ce23f315c2b

--- paella/player-develop/resources/deps/bg2e.js
+++ paella/player-develop/resources/deps/bg2e.js
@@ -329,8 +329,12 @@ Reflect.defineProperty = Reflect.defineProperty || Object.defineProperty;
     function Resource() {}
     return ($traceurRuntime.createClass)(Resource, {}, {
       GetExtension: function(url) {
-        var match = /\.([a-z0-9-_]*)$/i.exec(url);
-        return (match && match[1].toLowerCase()) || "";
+        var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0].toLowerCase();
+        if (filename.indexOf(".") > 0) {
+          return filename.split(".").pop().toLowerCase();
+        } else {
+          return "";
+        }
       },
       IsFormat: function(url, formats) {
         return formats.find(function(fmt) {
@@ -11807,12 +11811,6 @@ bg.webgl1 = {};
       },
       destroy: function(context, texture) {
         context.deleteTexture(this._texture);
-      }
-    }, {}, $__super);
-  }(bg.base.TextureImpl);
-  bg.webgl1.TextureImpl = TextureImpl;
-})();
-.deleteTexture(this._texture);
       }
     }, {}, $__super);
   }(bg.base.TextureImpl);
