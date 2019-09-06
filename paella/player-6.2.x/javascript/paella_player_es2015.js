ffca6797b0dacbd190d676b79a00982787a0cc2b
Fernando Serrano Carpena <ferserc1@gmail.com>
2019-09-06 09:21:51 +0000

    Update paella player 6.2.x
    Triggered by https://github.com/polimediaupv/paella/commit/2f0d17017bb166a2290944de01c973cd8584e37c

--- paella/player-6.2.x/javascript/paella_player_es2015.js
+++ paella/player-6.2.x/javascript/paella_player_es2015.js
@@ -22,7 +22,7 @@ var GlobalParams = {
 
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.2.0 - build: 4193605";
+paella.version = "6.2.0 - build: 2f0d170";
 
 (function buildBaseUrl() {
 	if (window.paella_debug_baseUrl) {
