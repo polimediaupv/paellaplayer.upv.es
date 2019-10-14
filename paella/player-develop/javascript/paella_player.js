09548dc77bd7ec11e4e59ec1b4871c1fa9816780
Fernando Serrano Carpena <ferserc1@gmail.com>
2019-10-14 11:40:07 +0000

    Update paella player develop
    Triggered by https://github.com/polimediaupv/paella/commit/3c1004f1d4cc79abe3142b4902cd3f217c7f48f1

--- paella/player-develop/javascript/paella_player.js
+++ paella/player-develop/javascript/paella_player.js
@@ -47,7 +47,7 @@ var GlobalParams = {
 };
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.3.0 - build: 38c520b";
+paella.version = "6.3.0 - build: 3c1004f";
 
 (function buildBaseUrl() {
   if (window.paella_debug_baseUrl) {
@@ -10437,46 +10437,49 @@ function paella_DeferredNotImplemented() {
       container.style.pointerEvents = "none";
       document.body.appendChild(container);
       _this119.container = container;
-      var icon = document.createElement('canvas');
-      icon.style.width = "100%";
-      icon.style.height = "300px";
-      icon.style.display = "block";
-      icon.style.cursor = "pointer";
-      container.appendChild(icon);
 
-      function repaintCanvas() {
-        var width = jQuery(container).innerWidth();
-        var height = jQuery(container).innerHeight();
-        icon.style.marginTop = "".concat(window.innerHeight / 2 - 150, "px");
-        icon.width = width;
-        icon.height = height;
-        var ratio = width / height;
-        var iconWidth = width < height ? width / 3 : height / 3;
-        var iconHeight = iconWidth;
-        var ctx = icon.getContext('2d'); // Play Icon size: 300x300
+      if (!paella.player.videoContainer) {
+        var repaintCanvas = function repaintCanvas() {
+          var width = jQuery(container).innerWidth();
+          var height = jQuery(container).innerHeight();
+          icon.style.marginTop = "".concat(window.innerHeight / 2 - 150, "px");
+          icon.width = width;
+          icon.height = height;
+          var ratio = width / height;
+          var iconWidth = width < height ? width / 3 : height / 3;
+          var iconHeight = iconWidth;
+          var ctx = icon.getContext('2d'); // Play Icon size: 300x300
 
-        ctx.translate((width - iconWidth) / 2, (height - iconHeight) / 2);
-        ctx.beginPath();
-        ctx.arc(iconWidth / 2, iconHeight / 2, iconWidth / 2, 0, 2 * Math.PI, true);
-        ctx.closePath();
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = 10;
-        ctx.stroke();
-        ctx.fillStyle = '#8f8f8f';
-        ctx.fill();
-        ctx.beginPath();
-        ctx.moveTo(iconWidth / 3, iconHeight / 4);
-        ctx.lineTo(3 * iconWidth / 4, iconHeight / 2);
-        ctx.lineTo(iconWidth / 3, 3 * iconHeight / 4);
-        ctx.lineTo(iconWidth / 3, iconHeight / 4);
-        ctx.closePath();
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.stroke();
-      } //paella.events.bind(paella.events.resize,repaintCanvas);
+          ctx.translate((width - iconWidth) / 2, (height - iconHeight) / 2);
+          ctx.beginPath();
+          ctx.arc(iconWidth / 2, iconHeight / 2, iconWidth / 2, 0, 2 * Math.PI, true);
+          ctx.closePath();
+          ctx.strokeStyle = 'white';
+          ctx.lineWidth = 10;
+          ctx.stroke();
+          ctx.fillStyle = '#8f8f8f';
+          ctx.fill();
+          ctx.beginPath();
+          ctx.moveTo(iconWidth / 3, iconHeight / 4);
+          ctx.lineTo(3 * iconWidth / 4, iconHeight / 2);
+          ctx.lineTo(iconWidth / 3, 3 * iconHeight / 4);
+          ctx.lineTo(iconWidth / 3, iconHeight / 4);
+          ctx.closePath();
+          ctx.fillStyle = 'white';
+          ctx.fill();
+          ctx.stroke();
+        }; //paella.events.bind(paella.events.resize,repaintCanvas);
 
 
-      repaintCanvas();
+        var icon = document.createElement('canvas');
+        icon.style.width = "100%";
+        icon.style.height = "300px";
+        icon.style.display = "block";
+        icon.style.cursor = "pointer";
+        container.appendChild(icon);
+        repaintCanvas();
+      }
+
       return _this119;
     }
 
