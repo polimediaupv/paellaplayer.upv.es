e6eb8f3d361414aa4f1e3b05c714d4e47a7c4503
Miguel Escriva <miesgre@gmail.com>
2019-11-05 12:41:07 +0000

    Update paella player 6.3.x
    Triggered by https://github.com/polimediaupv/paella/commit/de739b963972c2d19b8daa7d264fa7a49f1c4c29

--- paella/player-6.3.x/javascript/paella_player_es2015.js
+++ paella/player-6.3.x/javascript/paella_player_es2015.js
@@ -22,7 +22,7 @@ var GlobalParams = {
 
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.3.0 - build: 67db370";
+paella.version = "6.3.0 - build: de739b9";
 
 (function buildBaseUrl() {
 	if (window.paella_debug_baseUrl) {
