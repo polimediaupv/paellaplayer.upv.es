163f765014fea82160487969eaac22eeb128d64d
Miguel Escrivá Gregori <miesgre@gmail.com>
2020-05-20 18:02:05 +0000

    Update paella player 6.4.x
    Triggered by https://github.com/polimediaupv/paella/commit/b9979a0ff5116af7300384f0bd77cde1a14949d0

--- paella/player-6.4.x/javascript/paella_player.js
+++ paella/player-6.4.x/javascript/paella_player.js
@@ -63,7 +63,7 @@ var GlobalParams = {
 };
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.4.3 - build: d53b311";
+paella.version = "6.4.3 - build: b9979a0";
 
 (function buildBaseUrl() {
   if (window.paella_debug_baseUrl) {
