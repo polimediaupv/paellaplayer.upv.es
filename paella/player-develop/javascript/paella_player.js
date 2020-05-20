9924134ef6859a85add356abb90267a2d8fa3e30
Miguel Escrivá Gregori <miesgre@gmail.com>
2020-05-20 18:09:25 +0000

    Update paella player develop
    Triggered by https://github.com/polimediaupv/paella/commit/848e0c1d9d839f3c9c6d14b6e4322903791cf521

--- paella/player-develop/javascript/paella_player.js
+++ paella/player-develop/javascript/paella_player.js
@@ -65,7 +65,7 @@ var GlobalParams = {
 };
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.5.0 - build: 17dead0";
+paella.version = "6.5.0 - build: 848e0c1";
 
 (function buildBaseUrl() {
   if (window.paella_debug_baseUrl) {
