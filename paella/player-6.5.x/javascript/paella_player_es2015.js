cf6884b1d261bf22df9159190027935f311ca46b
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-04-14 07:55:43 +0000

    Update paella player 6.5.x
    Triggered by https://github.com/polimediaupv/paella/commit/defeb41dbe6e404c45f1df36163db6c2671214b1

--- paella/player-6.5.x/javascript/paella_player_es2015.js
+++ paella/player-6.5.x/javascript/paella_player_es2015.js
@@ -22,7 +22,7 @@ var GlobalParams = {
 
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.5.3 - build: 8cc956b";
+paella.version = "6.5.3 - build: defeb41";
 
 (function buildBaseUrl() {
 	if (window.paella_debug_baseUrl) {
@@ -75,8 +75,6 @@ paella.events = {
 	videoUnloaded:'paella:videoUnloaded',
 	
 	controlBarLoaded:'paella:controlBarLoaded',	
-	
-	flashVideoEvent:'paella:flashVideoEvent',
 	
 	captionAdded: 'paella:caption:add', // Event triggered when new caption is available.
 	captionsEnabled: 'paella:caption:enabled',  // Event triguered when a caption es enabled.
@@ -10622,8 +10620,15 @@ paella.addPlugin(function() {
 		getName() { return "edu.harvard.dce.paella.flexSkipPlugin"; }
 		getIndex() { return 121; }
 		getSubclass() { return 'flexSkip_Rewind_10'; }
-		getIconClass() { return 'icon-back-10-s'; }
-		formatMessage() { return 'Rewind 10 seconds'; }
+		getIconClass() {
+			if (this.config.seconds === 10) {
+				return 'icon-back-10-s';
+			}
+			else {
+				return 'icon-backward';
+			}
+		}
+		formatMessage() { return `Rewind ${ this.config.seconds } seconds`; }
 		getDefaultToolTip() { return paella.utils.dictionary.translate(this.formatMessage()); }
 	
 		checkEnabled(onSuccess) {
@@ -10632,8 +10637,8 @@ paella.addPlugin(function() {
 		
 		action(button) {
 			paella.player.videoContainer.currentTime()
-				.then(function(currentTime) {
-					paella.player.videoContainer.seekToTime(currentTime - 10);
+				.then((currentTime) => {
+					paella.player.videoContainer.seekToTime(currentTime - this.config.seconds);
 				});
 		}
 	}
@@ -10649,13 +10654,20 @@ paella.addPlugin(function() {
 		getIndex() { return 122; }
 		getName() { return "edu.harvard.dce.paella.flexSkipForwardPlugin"; }
 		getSubclass() { return 'flexSkip_Forward_30'; }
-		getIconClass() { return 'icon-forward-30-s'; }
-		formatMessage() { return 'Forward 30 seconds'; }
+		getIconClass() { 
+			if (this.config.seconds === 30) {
+				return 'icon-forward-30-s';
+			}
+			else {
+				return 'icon-forward2';
+			}
+		}
+		formatMessage() { return `Forward ${this.config.seconds} seconds`; }
 		
 		action(button) {
 			paella.player.videoContainer.currentTime()
-				.then(function(currentTime) {
-					paella.player.videoContainer.seekToTime(currentTime + 30);
+				.then((currentTime) => {
+					paella.player.videoContainer.seekToTime(currentTime + this.config.seconds);
 				});
 		}
 	}
