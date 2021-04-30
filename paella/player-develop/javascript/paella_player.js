785e21208cc6e989101384ed312925da4d14036b
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-04-30 12:19:21 +0000

    Update paella player develop
    Triggered by https://github.com/polimediaupv/paella/commit/ec3420a76295fbab37c8bee95908030f0e37671a

--- paella/player-develop/javascript/paella_player.js
+++ paella/player-develop/javascript/paella_player.js
@@ -69,7 +69,7 @@ var GlobalParams = {
 };
 window.paella = window.paella || {};
 paella.player = null;
-paella.version = "6.5.4 - build: 3bc5e7d";
+paella.version = "6.5.4 - build: ec3420a";
 
 (function buildBaseUrl() {
   if (window.paella_debug_baseUrl) {
