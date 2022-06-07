9defdc096495cfbcc7f473c84340bce0b170948a
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-06-07 12:21:17 +0200

    Header updated

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -28,26 +28,6 @@
             </div>
             <div class="download-group">
                 <a href="#/download" class="linkButton" id="downloadPaella3">Download<br></a>
-                <div style="margin-top:3px;">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="98" height="18">
-                        <linearGradient id="a" x2="0" y2="100%">
-                            <stop offset="0" stop-color="#fff" stop-opacity=".7"></stop>
-                            <stop offset=".1" stop-color="#aaa" stop-opacity=".1"></stop>
-                            <stop offset=".9" stop-opacity=".3"></stop>
-                            <stop offset="1" stop-opacity=".5"></stop>
-                        </linearGradient>
-                        <rect rx="4" width="98" height="18" fill="#555"></rect>
-                        <rect rx="4" x="52" width="46" height="18" fill="#97CA00"></rect>
-                        <path fill="#97CA00" d="M52 0h4v18h-4z"></path>
-                        <rect rx="4" width="98" height="18" fill="url(#a)"></rect>
-                        <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-                            <text x="27" y="14" fill="#010101" fill-opacity=".3">version</text>
-                            <text x="27" y="13">version</text>
-                            <text x="74.5" y="14" fill="#010101" fill-opacity=".3">{paellaCoreVersion}</text>
-                            <text x="74.5" y="13">{paellaCoreVersion}</text>
-                        </g>
-                    </svg>
-                </div>
             </div>
         
             <div class="menu">
