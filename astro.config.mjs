d4b106e8b1017102ce3a19cd7a28542013d07d56
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-04-11 14:17:15 +0200

    Github repo updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig({
 				replacesTitle: true,
 			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/polimediaupv/paella-player'
 			},
 			sidebar: [
 				{
