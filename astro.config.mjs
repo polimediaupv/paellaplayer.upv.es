8f718895981a89387b96bc6e3fd52483112b731d
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-06-18 13:52:08 +0200

    node integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,61 +3,70 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://paellaplayer.upv.es',
-	security: {
-		checkOrigin: false
+  site: 'https://paellaplayer.upv.es',
+
+  security: {
+      checkOrigin: false
 	},
-	vite: {
-		plugins: [tailwindcss()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	integrations: [
-		starlight({
-			title: 'Documentation',
-			customCss: [
-				'./src/styles/custom.css'
-			],
-			logo: {
-				light: './src/assets/light-logo.webp',
-				dark: './src/assets/dark-logo.webp',
-				replacesTitle: true,
-			},
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/polimediaupv/paella-player'
-				}
-			],
-			sidebar: [
-				{
-					label: 'Tutorial',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Vanilla Player', slug: 'tutorial/vanilla' },
-						{ label: 'Quick Start', slug: 'tutorial/quick_start' },
-						{ label: 'Two Streams', slug: 'tutorial/two_streams' },
-						{ label: 'Init Params', slug: 'tutorial/init_params' },
-						{ label: 'Plugins', slug: 'tutorial/plugins' },
-						{ label: 'Group Buttons', slug: 'tutorial/group_buttons' },
-						{ label: 'Styling', slug: 'tutorial/styling' },
-						{ label: 'Skins', slug: 'tutorial/skins' }
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Integrations',
-					autogenerate: { directory: 'integrations' },
-				},
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				}
-			],
-		}),
+
+  integrations: [
+      starlight({
+          title: 'Documentation',
+          customCss: [
+              './src/styles/custom.css'
+          ],
+          logo: {
+              light: './src/assets/light-logo.webp',
+              dark: './src/assets/dark-logo.webp',
+              replacesTitle: true,
+          },
+          social: [
+              {
+                  icon: 'github',
+                  label: 'GitHub',
+                  href: 'https://github.com/polimediaupv/paella-player'
+              }
+          ],
+          sidebar: [
+              {
+                  label: 'Tutorial',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Vanilla Player', slug: 'tutorial/vanilla' },
+                      { label: 'Quick Start', slug: 'tutorial/quick_start' },
+                      { label: 'Two Streams', slug: 'tutorial/two_streams' },
+                      { label: 'Init Params', slug: 'tutorial/init_params' },
+                      { label: 'Plugins', slug: 'tutorial/plugins' },
+                      { label: 'Group Buttons', slug: 'tutorial/group_buttons' },
+                      { label: 'Styling', slug: 'tutorial/styling' },
+                      { label: 'Skins', slug: 'tutorial/skins' }
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+              {
+                  label: 'Integrations',
+                  autogenerate: { directory: 'integrations' },
+              },
+              {
+                  label: 'Guides',
+                  autogenerate: { directory: 'guides' },
+              }
+          ],
+      }),
 	],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
