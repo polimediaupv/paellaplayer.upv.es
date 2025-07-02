9da2bc6a020accca15840eb7942ad13e3a6aac2e
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-07-02 09:20:37 +0200

    Improvements in documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,67 +7,92 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://paellaplayer.webs.upv.es',
+    site: 'https://paellaplayer.webs.upv.es',
 
-  security: {
-      checkOrigin: false
-	},
+    security: {
+        checkOrigin: false
+    },
 
-  vite: {
-      plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [
-      starlight({
-          title: 'Documentation',
-          customCss: [
-              './src/styles/custom.css'
-          ],
-          logo: {
-              light: './src/assets/light-logo.webp',
-              dark: './src/assets/dark-logo.webp',
-              replacesTitle: true,
-          },
-          social: [
-              {
-                  icon: 'github',
-                  label: 'GitHub',
-                  href: 'https://github.com/polimediaupv/paella-player'
-              }
-          ],
-          prerender: false,
-          sidebar: [
-              {
-                  label: 'Tutorial',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Vanilla Player', slug: 'tutorial/vanilla' },
-                      { label: 'Quick Start', slug: 'tutorial/quick_start' },
-                      { label: 'Two Streams', slug: 'tutorial/two_streams' },
-                      { label: 'Init Params', slug: 'tutorial/init_params' },
-                      { label: 'Plugins', slug: 'tutorial/plugins' },
-                      { label: 'Group Buttons', slug: 'tutorial/group_buttons' },
-                      { label: 'Styling', slug: 'tutorial/styling' },
-                      { label: 'Skins', slug: 'tutorial/skins' }
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-              {
-                  label: 'Integrations',
-                  autogenerate: { directory: 'integrations' },
-              },
-              {
-                  label: 'Guides',
-                  autogenerate: { directory: 'guides' },
-              }
-          ],
-      }),
-	],
+    integrations: [
+        starlight({
+            title: 'Documentation',
+            customCss: [
+                './src/styles/custom.css'
+            ],
+            logo: {
+                light: './src/assets/light-logo.webp',
+                dark: './src/assets/dark-logo.webp',
+                replacesTitle: true,
+            },
+            social: [
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/polimediaupv/paella-player'
+                }
+            ],
+            /*prerender: false,*/
+            sidebar: [
+                {
+                    label: 'Tutorial',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Vanilla Player', slug: 'tutorial/vanilla' },
+                        { label: 'Quick Start', slug: 'tutorial/quick_start' },
+                        { label: 'Two Streams', slug: 'tutorial/two_streams' },
+                        { label: 'Init Params', slug: 'tutorial/init_params' },
+                        { label: 'Plugins', slug: 'tutorial/plugins' },
+                        { label: 'Group Buttons', slug: 'tutorial/group_buttons' },
+                        { label: 'Styling', slug: 'tutorial/styling' },
+                        { label: 'Skins', slug: 'tutorial/skins' }
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Plugins',
+                    items: [
+                        { label: 'Paella Player Plugins', slug: 'plugins/plugins'},
+                        { label: 'Plugin Modules', slug: 'plugins/plugin_modules' },
+                        { label: 'Button Plugins', slug: 'plugins/button_plugins' },
+                        { label: 'Pop Up Plugins', slug: 'plugins/pop_up_button_plugins' },
+                        { label: 'Menu Button Plugins', slug: 'plugins/menu_button_plugins' },
+                        { label: 'Canvas Button Plugins', slug: 'plugins/canvas_button_plugins' },
+                        { label: 'Canvas Plugins', slug: 'plugins/canvas_plugins' },
+                        { label: 'Video Plugins', slug: 'plugins/video_plugins' },
+                        { label: 'Video Layout Plugins', slug: 'plugins/video_layout' },
+                        { label: 'Data Plugins', slug: 'plugins/data_plugins' },
+                        { label: 'Event Log Plugins', slug: 'plugins/event_log_plugins' },
+                        { label: 'Plugin pacakges', items: [
+                            { label: 'Core Plugins', slug: 'plugins/plugins_core' },
+                            { label: 'Basic Plugins', slug: 'plugins/plugins_basic' },
+                            { label: 'Slide Plugins', slug: 'plugins/plugins_slides' },
+                            { label: 'Zoom Plugin', slug: 'plugins/plugins_zoom' },
+                            { label: 'User Tracking Plugins', slug: 'plugins/plugins_user_tracking' },
+                            { label: 'Video Plugins', slug: 'plugins/plugins_video' },
+                            { label: 'WebGL Plugins', slug: 'plugins/plugins_webgl' }
+                        ]}
+                    ]
+                },
+                {
+                    label: 'Integrations',
+                    autogenerate: { directory: 'integrations' },
+                },
+                {
+                    label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+                }
+            ],
+        }),
+    ],
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
